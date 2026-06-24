15c77a2b4bf72c02eeaf9ad2845b2126202b689d
kush dev <36306452+kushdab@users.noreply.github.com>
2026-06-24 10:33:20 -0600

    Initial commit: full implementation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'mpesa-green': '#49aa34',
+      }
+    },
+  },
+  plugins: [],
+}
